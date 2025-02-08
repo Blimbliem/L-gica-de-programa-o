0325ab9b52b381e64d402eb30a8454db8a8b724d
Blimbliem <studyblimblim@gmail.com>
2025-02-07 21:03:05 -0300

    Primeira aula de operadores relacionais

---
+++ Operadores de comparação/OperadoresRelacionais.js
@@ -0,0 +1,16 @@
+let numero = "1"
+console.log(numero == 1)
+console.log(numero === 1)
+
+// = é uma atribuição
+// == é para comparar o valor
+// === é para comparar o valor e o formato do conteúdo
+
+
+//Guardar os valores em uma variavel de resultado.
+let marca = "Apple"
+resultado = marca !=="Samsung"
+
+console.log(resultado)
+
+// !== é para diferenciar
